ca26be34d1496f1fa96a76b4135ddefa397960b4
saranobyl01 <saranobyl01@gmail.com>
2025-12-24 05:38:09 +0530

    Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,8 @@ import NotFound from "./pages/NotFound";
 
 import ProductDetail from "./pages/ProductDetail";
 
+import ScrollToTop from "@/components/ScrollToTop";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -20,6 +22,7 @@ const App = () => (
           <Toaster />
           <Sonner />
           <BrowserRouter>
+            <ScrollToTop />
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/product/:id" element={<ProductDetail />} />
